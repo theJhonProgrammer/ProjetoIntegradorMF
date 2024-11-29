593f173eed1ff19d02768d62a4644adbd0f2d187
SNunesA <snunesadiers@gmail.com>
2024-11-29 17:26:39 -0300

    logintoken

--- api_cadastro/usuarios/views.py
+++ api_cadastro/usuarios/views.py
@@ -7,8 +7,7 @@ from django.contrib.auth.models import User
 from .serializers import UsuarioSerializer
 from rest_framework.permissions import AllowAny 
 from rest_framework.authtoken.models import Token
-from rest_framework.authtoken.views import obtain_auth_token
-
+from rest_framework.authtoken.views import ObtainAuthToken
 from django.shortcuts import render
 
 
@@ -33,9 +32,22 @@ class UsuarioList(APIView):
         serializer = UsuarioSerializer(usuarios, many=True)
         return Response(serializer.data)
 
-class LoginView(APIView):
+class LoginView(ObtainAuthToken):
     permission_classes = [AllowAny] # Permite acesso público a este endpoint
     def get(self, request):
         return render(request, 'usuarios/login.html')
     def post(self, request, *args, **kwargs):
-        return obtain_auth_token
+        # Use a classe serializer da base
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+
+        # Pegue o usuário validado
+        user = serializer.validated_data['user']
+        
+        # Gere ou recupere o token para o usuário
+        token, created = Token.objects.get_or_create(user=user)
+
+        # Retorne a resposta com os dados do token
+        return Response({
+            'token': token.key
+        })
